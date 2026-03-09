b23e6ced79c118849dcff5c156435a7dd66b5dca
David Ogbonnah <davido6593@gmail.com>
2026-03-09 02:03:22 +0000

    Update TF Bucket Name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "xxxxx-tfstate"
+    bucket       = "dcopro-tfstate"
     key          = "tf-gitlab-runner/terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true
